eb7e73796b70828f09c479771c35e8829239ab41
Daniel <d15_1_89@msn.com>
2024-10-25 17:36:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import ListComponent from './components/list/list';
+
+function App() {
+  return (
+    <div className="App">
+      <ListComponent />
+    </div>
+  );
+}
+
+export default App;

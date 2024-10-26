4ed8da2eeb9b70a2d31d049a5747e323cb774f5c
Daniel <d15_1_89@msn.com>
2024-10-26 10:10:52 +0200

    sorting and filtering

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCharacters } from '../../features/rickAndMortyActions';
 import { CharacterType } from '../../interfaces/character';
@@ -14,22 +14,88 @@ const ListComponent: React.FC = () => {
   const isLoading = useSelector(
     (state: RootState) => state.characters.isLoading
   );
+  const [sortedCharacters, setSortedCharacters] = useState<CharacterType[]>([]);
+  const [nameSort, setNameSort] = useState<'asc' | 'desc' | null>(null);
+  const [statusFilter, setStatusFilter] = useState<'alive' | 'dead' | 'any'>(
+    'any'
+  );
 
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (characters.results) {
+      const sorted = [...characters.results];
+      setSortedCharacters(sorted);
+    }
+  }, [characters.results]);
+
+  const sortByName = () => {
+    const sorted = [...sortedCharacters].sort((a, b) => {
+      if (nameSort === 'asc') {
+        return a.name.localeCompare(b.name);
+      } else {
+        return b.name.localeCompare(a.name);
+      }
+    });
+    setSortedCharacters(sorted);
+    setNameSort(nameSort === 'asc' ? 'desc' : 'asc');
+  };
+
+  const filterByStatus = (status: 'alive' | 'dead' | 'any') => {
+    setStatusFilter(status);
+    if (status === 'any') {
+      setSortedCharacters([...characters.results]);
+    } else {
+      const filtered = characters.results.filter(
+        (character) => character.status.toLowerCase() === status
+      );
+      setSortedCharacters(filtered);
+    }
+  };
+
   return (
     <div>
       <h1>Rick & Morty</h1>
+      <div className="filter">
+        Sort by:
+        <div className="filter-element" onClick={sortByName}>
+          Name{' '}
+          {nameSort === 'asc' ? (
+            <span>&#8593;</span>
+          ) : nameSort === 'desc' ? (
+            <span>&#8595;</span>
+          ) : null}
+        </div>
+      </div>
+      <div className="filter">
+        Order by Status:
+        <div className="filter-element">
+          <span
+            className={`status-dot ${statusFilter === 'alive' ? 'active' : ''}`}
+            style={{ backgroundColor: 'rgb(61, 214, 46)' }}
+            onClick={() => filterByStatus('alive')}
+          ></span>
+          <span
+            className={`status-dot ${statusFilter === 'dead' ? 'active' : ''}`}
+            style={{ backgroundColor: 'rgb(214, 61, 46)' }}
+            onClick={() => filterByStatus('dead')}
+          ></span>
+          <span
+            className={`status-dot ${statusFilter === 'any' ? 'active' : ''}`}
+            style={{ backgroundColor: 'grey' }}
+            onClick={() => filterByStatus('any')}
+          ></span>
+        </div>
+      </div>
       {isLoading ? (
         <SpinnerComponent />
       ) : (
         <section className="character-grid">
-          {characters.results &&
-            characters.results.map((character: CharacterType) => (
-              <CharacterComponent key={character.id} character={character} />
-            ))}
+          {sortedCharacters.map((character: CharacterType) => (
+            <CharacterComponent key={character.id} character={character} />
+          ))}
         </section>
       )}
       {error && <p>Error: {error}</p>}
